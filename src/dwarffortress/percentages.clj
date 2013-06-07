a5e38a919bfbf4d7557176a5dc8019ef2c3f8410
Steve Shogren <steve.a.shogren@gmail.com>
2013-06-07 17:06:58 -0400

    Make if-percent work as a macro, but still with side-effects

--- src/dwarffortress/percentages.clj
+++ src/dwarffortress/percentages.clj
@@ -9,36 +9,26 @@
           (range 100))))
 (make-percents)
 
-(defn if-percent [& x]
-  (if (= 1 (mod (count x) 2))
-    :fail
-    (let [roll (rand-int 100) 
-          pairs (partition 2 x)]
-      (if (= 100 (reduce + (filter pos? (map first pairs))))
-        (loop [ipairs pairs
-               total 0]
-          (let [[percent ret] (first ipairs)
-                next-tot (+ total percent)]
-            (if (<= roll next-tot)
-              ret
-              (recur (rest ipairs) next-tot))))
-        :fail))))
-
-(defmacro if-percent [& x]
-  `(if (= 1 (mod (count ~x) 2))
-    :fail
-    (let [roll# (rand-int 100) 
-          pairs# (partition 2 ~x)]
-      (if (= 100 (reduce + (filter pos? (map first pairs#))))
-        (loop [ipairs# pairs#
-               total# 0]
-          (let [[percent# ret#] (first ipairs#)
-                next-tot# (+ total# percent#)]
-            (if (<= roll# next-tot#)
-              ret#
-              (recur (rest ipairs#) next-tot#))))
-        :fail))))
+(defmacro if-percent [& n]
+  `(let [x# (list ~@n)]
+    (if (= 1 (mod (count x#) 2))
+      :fail
+      (let [roll# (rand-int 100) 
+            pairs# (partition 2 x#)]
+        (if (= 100 (reduce + (filter pos? (map first pairs#))))
+          (loop [ipairs# pairs#
+                 total# 0]
+            (let [[percent# ret#] (first ipairs#)
+                  next-tot# (+ total# percent#)]
+              (if (<= roll# next-tot#)
+                ret#
+                (recur (rest ipairs#) next-tot#))))
+          :fail)))))
 
 (= :c (with-redefs [rand-int (fn [x] 100)]
+        (if-percent 50 (print "fist") 49 (print "second") 1 :c)))
+
+(= :a (with-redefs [rand-int (fn [x] 50)]
         (if-percent 50 :a 49 :b 1 :c)))
+
 
