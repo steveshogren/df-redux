08f9e5f75eab1c42a386cc8ca97bb3839c2fa8e8
Steve Shogren <steve.a.shogren@gmail.com>
2013-06-24 17:59:52 -0400

    Roll macro refactoring Trying to refactor the rolling macro by changing the quote levels to solve the cast exception

--- src/dwarffortress/percentages.clj
+++ src/dwarffortress/percentages.clj
@@ -9,24 +9,44 @@
           (range 100))))
 (make-percents)
 
-(defmacro if-percent [& n]
-  `(let [x# (list ~@n)]
-    (if (= 1 (mod (count x#) 2))
-      :fail
-      (let [roll# (rand-int 100) 
-            pairs# (partition 2 x#)]
-        (if (= 100 (reduce + (filter pos? (map first pairs#))))
-          (loop [ipairs# pairs#
-                 total# 0]
-            (let [[percent# ret#] (first ipairs#)
-                  next-tot# (+ total# percent#)]
-              (if (<= roll# next-tot#)
-                ret#
-                (recur (rest ipairs#) next-tot#))))
-          :fail)))))
+(defmacro if-percent [& n#]
+  #_(print (partition 2 n#))
+  `(if (= 1 (mod (count ~n#) 2))
+     :fail ;; have to pass an even num of args
+     (let [roll# (rand-int 100) 
+           pairs# (partition 2 ~n#)]
+       (if (= 100 (reduce + (filter pos? (map first pairs#))))
+         (loop [ipairs# pairs#
+                total# 0]
+           (let [[percent# ret#] (first ipairs#)
+                 next-tot# (+ total# percent#)]
+             (if (<= roll# next-tot#)
+               (print ret#)
+               (recur (rest ipairs#) next-tot#))))
+         :fail)))) ;; nums have to = 100
+
+(defmacro if-percent [& n#]
+  #_(print (partition 2 n#))
+  (if (= 1 (mod (count n#) 2))
+     :fail ;; have to pass an even num of args
+     (let [pairs# (partition 2 n#)]
+       (if (= 100 (reduce + (filter pos? (map first pairs#))))
+         `(loop [ipairs# ~pairs#
+                roll# (rand-int 100)
+                total# 0]
+           (let [[percent# ret#] (first ipairs#)
+                 next-tot# (+ total# percent#)]
+             (if (<= roll# next-tot#)
+               ret#
+               (recur (rest ipairs#) roll# next-tot#))))
+         :fail)))) ;; nums have to = 100
+
+(defn yep [prt ret] (print prt) ret)
+(let [[a b] '(1 (test))]
+  b)
 
 (= :c (with-redefs [rand-int (fn [x] 100)]
-        (if-percent 50 (print "fist") 49 (print "second") 1 :c)))
+        (if-percent 50 (yep "first" :a) 49 (yep "second" :b) 1 :c)))
 
 (= :a (with-redefs [rand-int (fn [x] 50)]
         (if-percent 50 :a 49 :b 1 :c)))
