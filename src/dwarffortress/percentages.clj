fbd4ab54a4ab93ba7b191c9f054fc5a65c0c74c9
Steve Shogren <steve.a.shogren@gmail.com>
2013-06-25 11:09:00 -0400

    Movement to switch from a looping solution in the macro to a cond solution

--- src/dwarffortress/percentages.clj
+++ src/dwarffortress/percentages.clj
@@ -25,20 +25,36 @@
                (recur (rest ipairs#) next-tot#))))
          :fail)))) ;; nums have to = 100
 
+(defn sum-up-pair-percents [pairs]
+  (loop [sum 0
+         ipairs pairs
+         opairs []]
+    (print "ipairs:" ipairs "\n")
+    (print "opairs:" opairs "\n")
+    (let [[num val] (first ipairs)
+          next-sum (+ num sum)
+          updated-pair [next-sum val]]
+      (print "num: " num "\n")
+      (print "next-sum: " next-sum "\n")
+      (print "updated-pair: " updated-pair "\n")
+      (if (empty? ipairs)
+        updated-pair
+        (recur next-sum
+               (rest ipairs)
+               (cons updated-pair opairs))))))
+
+(sum-up-pair-percents '((50 :t) (50 :f)))
+
 (defmacro if-percent [& n#]
   #_(print (partition 2 n#))
   (if (= 1 (mod (count n#) 2))
      :fail ;; have to pass an even num of args
      (let [pairs# (partition 2 n#)]
        (if (= 100 (reduce + (filter pos? (map first pairs#))))
-         `(loop [ipairs# ~pairs#
-                roll# (rand-int 100)
-                total# 0]
-           (let [[percent# ret#] (first ipairs#)
-                 next-tot# (+ total# percent#)]
-             (if (<= roll# next-tot#)
-               ret#
-               (recur (rest ipairs#) roll# next-tot#))))
+         `(let [roll# (rand-int 100)
+                sum-pairs# (map )]
+            (cond
+             (list ~@(map (fn [pair#] (list (<= roll# )))) )))
          :fail)))) ;; nums have to = 100
 
 (defn yep [prt ret] (print prt) ret)
@@ -47,6 +63,7 @@
 
 (= :c (with-redefs [rand-int (fn [x] 100)]
         (if-percent 50 (yep "first" :a) 49 (yep "second" :b) 1 :c)))
+(pprint (macroexpand '(if-percent 50 (yep "first" :a) 49 (yep "second" :b) 1 :c)))
 
 (= :a (with-redefs [rand-int (fn [x] 50)]
         (if-percent 50 :a 49 :b 1 :c)))
