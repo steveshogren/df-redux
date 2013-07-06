8d3604fe7affadd28f462a8cdd29286db3eb94e3
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-06 12:21:45 -0400

    Work on drawing the board in swing

--- src/dwarffortress/core.clj
+++ src/dwarffortress/core.clj
@@ -1,25 +1,29 @@
 (ns dwarffortress.core
   (:use [clojure.test]
-        [dwarffortress.ui :only (draw-world)]
+        [dwarffortress.ui :only (get-world)]
         [dwarffortress.dwarf]
         [dwarffortress.map]
         [dwarffortress.percentages]
-        #_[seesaw.core])
+        [seesaw.core])
   (:gen-class))
+;;(use 'seesaw.core)
+;;(use 'seesaw.dev)
+;;(show-options (grid-panel))  
 
 (defn -main
   [& args]
-  #_(invoke-later
-   (-> (frame :title "heelo",
-              :content "hello seesaw",
-              :on-close :exit)
-       pack!
-       show!))
   (loop [map (make-map)
          dwarf (make-dwarf)
-         level 6]
-    (draw-world map dwarf level)
-    (let [inp (read-line)]
+         level 6
+         uimap (get-world map dwarf level)]
+    (invoke-now
+     (-> (frame :title "heelo",
+                :content (grid-panel :items uimap)
+                :minimum-size [400 :by 400]
+                :on-close :exit)
+         pack!
+         show!))
+    #_(let [inp (read-line)]
       (recur map (update-dwarf dwarf map) 
              (case inp
                "p" (if (= 9 level) level (+ 1 level))
--- src/dwarffortress/ui.clj
+++ src/dwarffortress/ui.clj
@@ -1,4 +1,5 @@
-(ns dwarffortress.ui)
+(ns dwarffortress.ui
+  (:use [dwarffortress.map]))
 
 (defn draw-ground [c]
   (case c
@@ -11,27 +12,22 @@
    (< 3 (:tired d)) "T"
    :else "D"))
 
-(defn add-cell-drawing [acc d x y level]
-  (str acc d (if (= y 9) "\n") (if (= x y 9) (str "\n" level "\n"))))
-
 (defn cell-match? [{x1 :x y1 :y z1 :z} {x2 :x y2 :y z2 :z}]
   (and (= x1 x2) (= y1 y2) (= z1 z2)))
 
+(defn this-level? [{z1 :z} z2] (= z1 z2))
+
+(defn get-world [w dwarf level]
+  (partition 10
+             (map (fn [cell]
+                    (if (cell-match? cell dwarf)
+                      (draw-dwarf dwarf)
+                      (draw-ground v)))
+                  (filter #(this-level? %1 level) w))))
+
+(get-world (make-map) {:x 1 :y 2 :z 1} 1)
+
 (defn draw-world [w dwarf level]
-  (println
-   (reduce (fn [acc cell]
-             (let [{x :x y :y z :z v :val} cell]
-               (if (= z level)
-                 (add-cell-drawing
-                  acc  
-                  (if (cell-match? cell dwarf)
-                    (draw-dwarf dwarf)
-                    (draw-ground v))
-                  x
-                  y
-                  level)
-                 acc)))
-           "" 
-           w)))
+  (map println (get-world w dwarf level)))
 
 
