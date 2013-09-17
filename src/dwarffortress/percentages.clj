92098759e021695d13540a4680758e6e16b6db7e
Steve Shogren <steve.a.shogren@gmail.com>
2013-09-17 12:35:22 -0400

    Finishing splitting out the if-percent macro Now, it really is just a wrapper cleaning up the syntax of the regular function, if-percent-fn

--- src/dwarffortress/percentages.clj
+++ src/dwarffortress/percentages.clj
@@ -1,5 +1,14 @@
 (ns dwarffortress.percentages)
 
+(defmacro _> [init & body]
+  "Used to anaphorically fill _ with the result of the previous expr
+   (_> 1 (+ 4 _) (+ _ 2) (* _ _)) => 49 
+   (_> (+ 1 1) (* _ _)) => 4" 
+  `(let [~'_ ~init
+         ~@(mapcat (fn [x] `[~'_ ~x])
+                   body)]
+     ~'_))
+
 (defn chance [x]
   (> x (rand-int 100)))
 
@@ -11,24 +20,8 @@
                  `(if (> ~~num (rand-int 100)) ~x# ~y#))))
           (range 100))))
 
-;;(sum-up-pair-percents '((50 (+ 1 1)) (50 :f))) 
-(defn sum-up-pair-percents [pairs]
-  "Macro helper that sums up the pairs of percents and results,
-   adding the total to each percent"
-  (loop [sum 0
-         ipairs pairs
-         opairs []]
-    (let [[num val] (first ipairs)
-          next-sum (+ num sum)
-          updated-pair (cons [next-sum val] opairs)]
-      (if (empty? (rest ipairs))
-        (reverse updated-pair)
-        (recur next-sum
-               (rest ipairs)
-               updated-pair)))))
-
 (defn if-percent-fn [& n]
-  (if (= 1 (mod (count n) 2))
+  (if (odd? (count n))
     (throw (Exception. "Must pass even num of args"))
     (let [pairs (partition 2 n)
           sum (reduce + (filter pos? (map first pairs)))]
@@ -40,29 +33,23 @@
                   current-sum (+ current-sum next-percent)]
               (if (<= roll current-sum)
                 (next-val)
-                (if (> current-sum 100)
-                  (throw (Exception. (str "Something didn't add up")))
-                  (recur current-sum (rest items)))))))
+                (recur current-sum (rest items))))))
         (throw (Exception. (str "Nums: " sum " didn't equal 100" )))))))
-
-(defn wrap-in-fn [x]
-  (fn [] x))
-
-(defn pair-flatten [col]
-  "(pair-flatten [[1 [2]] [3 4] [5 6]]) -> [1 [2] 3 4 5 6]"
-  (reduce (fn [items [x y]] (conj items x y))
-          []
-          col))
 
 (defmacro if-percent [& n]
   "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%"
-  (if (= 0 (mod (count n) 2))
+  (if (even? (count n))
     (let [pairs (partition 2 n)
-          pairs (mapcat (fn [x] [(first x) (fn [] (second x))]) pairs)]
+          pairs (mapcat (fn [[pct act]] [pct `(fn [] ~act)]) pairs)]
       `(if-percent-fn ~@pairs))))
 
-(pprint (macroexpand '(if-percent 50 (+ 1 1) 50 :b)))
-(if-percent 50 (+ 1 1) 50 (+ 2 2))
+#_(float (average (map (fn [x] (if-percent 50 0 50 10))
+                 (range 100000))))
+
+(defn average [col]
+  (let [sum (reduce + col)
+        count (count col)]
+    (/ sum count)))
 
 ;; Transliteration of PG's aif
 ;; Anaphoric macro that binds to "it" the test result
@@ -70,31 +57,24 @@
   `(let [~'it ~test]
      (if ~'it ~then ~else)))
 
-(aif (= it 2)
+#_(aif (= it 2)
      (do (print (str "it: " it)) it)
      :false)
 
 ;; Transliteration of PG's alambda
+;; Anaphoric macro that binds to "self" the function for recursion
+;; in typical Clojure, this should be achieved with loop/recur
 (defmacro alambda [parms & body]
   `(letfn [(~'self ~parms ~@body)]
      ~'self))
-((alambda [a b] (+ a b)) 1 2)
+#_((alambda [a b] (+ a b)) 1 2)
 
-((alambda [n]
+#_((alambda [n]
      (if (> n 0)
        (cons
         n
         (self (- n 1))))) 5)
 
-(defmacro _> [init & body]
-  "Used to anaphorically fill _ with the result of the previous expr
-   (_> 1 (+ 4 _) (+ _ 2) (* _ _)) => 49 
-   (_> (+ 1 1) (* _ _)) => 4" 
-  `(let [~'_ ~init
-         ~@(mapcat (fn [x]
-                     `[~'_ ~x])
-                   body)]
-     ~'_))
 
 
 
