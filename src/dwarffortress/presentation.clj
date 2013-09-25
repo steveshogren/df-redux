f61e250250a8d5cb693f19ae8ddcfdb0d11a18e7
Steve Shogren <steve.a.shogren@gmail.com>
2013-09-25 16:15:14 -0400

    Adding a document to use for the presentation

---
+++ src/dwarffortress/presentation.clj
@@ -0,0 +1,144 @@
+(ns dwarffortress.presentation)
+
+;; Think about templates for a second...
+<html>
+  <p>
+  <?php> if(today_day() == "Monday") { echo("Mon")} <?/php>
+  </p>
+</html>
+
+<html>
+  <p>
+  <% if todayDay() eq "Monday" then return "Mon" end %>
+  </p>
+</html>
+
+;;They let you "disable/enable" the evaulation of code
+
+;; no quote                +   => #<core$_PLUS_ clojure.core$_PLUS_@d3d424c>
+;; ' => quote             '+   => +
+
+;; ` => syntax quote      `+   => clojure.core/+
+;; ~ => unquote           `~+  => #<core$_PLUS_ clojure.core$_PLUS_@d3d424c>
+;; ~@ => unquote splicing
+;;    `(~@[1 3] 1) => (1 3 1)
+;;    `(~@[+ 3] 1) => (#<core$_PLUS_ clojure.core$_PLUS_@d3d424c> 3 1)
+
+;; Function
+(defn add [x y]
+  (+ x y))
+;; (add (+ 1 1) 3) => 5
+
+;; "Replaced"
+(defn add [2 3]
+  (+ 2 3))
+
+
+;; Macro 
+(defmacro add [x y]
+  `(+ ~x ~y))
+;; (add (+ 1 1) 3) => 5
+
+;; After "replacing" the values
+(defmacro add [(+ 1 1) 3]
+  `(+ (+ 1 1) 3))
+;; (macroexpand '(add (+ 1 1) 3)) => (clojure.core/+ (+ 1 1) 3)
+
+
+;; Macros allow for the expansion of code -without- the
+;; increase in stack depth and associated attributes
+
+
+;; Anaphoric Macros
+(defmacro add-weird? [x]
+  `(let [~'z 100]
+     ~x))
+(let [z 1]
+  (add-weird? (+ 1 z))) ;; => 101 ...not 2 
+
+;; Anaphoric macros allow the deliberate shadowing of
+;; values when expanded. While dangerous, this can be
+;; very useful in certain circumstances
+
+
+;; _> is meant to "enhance" the value of the normal ->
+;; command by allowing a Scala-like syntax
+(defmacro _> [init & body]
+  "Used to anaphorically replace _ with the result of the previous expr
+   (_> 1 (+ 4 _) (+ _ 2) (* _ _)) => 49 
+   (_> (+ 1 1) (* _ _)) => 4" 
+  `(let [~'_ ~init
+         ~@(mapcat (fn [x] `[~'_ ~x])
+                   body)]
+     ~'_))
+
+
+(defn chance [x]
+  (> x (rand-int 100)))
+
+(defmacro make-percents []
+  "(ifN x y) returns x N% of the time, but ensures conditional evaluation, like 'if'"
+  `(list ~@(map (fn [num] 
+            (let [macro-name (symbol (str "if" num))]
+              `(defmacro ~macro-name [x# y#]
+                 `(if (> ~~num (rand-int 100)) ~x# ~y#))))
+          (range 100))))
+
+(defn if-percent-fn [& n]
+  (if (odd? (count n))
+    (throw (Exception. "Must pass even num of args"))
+    (let [pairs (partition 2 n)
+          sum (reduce + (filter pos? (map first pairs)))]
+      (if (= 100 sum)
+        (let [roll (rand-int 100)]
+          (loop [current-sum 0
+                 items pairs]
+            (let [[next-percent next-val] (first items)
+                  current-sum (+ current-sum next-percent)]
+              (if (<= roll current-sum)
+                (next-val)
+                (recur current-sum (rest items))))))
+        (throw (Exception. (str "Nums: " sum " didn't equal 100" )))))))
+
+(defmacro if-percent [& n]
+  "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%"
+  (if (even? (count n))
+    (let [pairs (partition 2 n)
+          pairs (mapcat (fn [[pct act]] [pct `(fn [] ~act)]) pairs)]
+      `(if-percent-fn ~@pairs))))
+
+#_(float (average (map (fn [x] (if-percent 50 0 50 10))
+                 (range 100000))))
+
+(defn average [col]
+  (let [sum (reduce + col)
+        count (count col)]
+    (/ sum count)))
+
+;; Transliteration of PG's aif
+;; Anaphoric macro that binds to "it" the test result
+(defmacro aif [test then else]
+  `(let [~'it ~test]
+     (if ~'it ~then ~else)))
+
+#_(aif (= it 2)
+     (do (print (str "it: " it)) it)
+     :false)
+
+;; Transliteration of PG's alambda
+;; Anaphoric macro that binds to "self" the function for recursion
+;; in typical Clojure, this should be achieved with loop/recur
+(defmacro alambda [parms & body]
+  `(letfn [(~'self ~parms ~@body)]
+     ~'self))
+#_((alambda [a b] (+ a b)) 1 2)
+
+#_((alambda [n]
+     (if (> n 0)
+       (cons
+        n
+        (self (- n 1))))) 5)
+
+
+
+
