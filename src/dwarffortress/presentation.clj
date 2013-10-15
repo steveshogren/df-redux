a4960b41b4ed4c49dd7d454395db7a64b94b3e1c
Steve Shogren <steve.a.shogren@gmail.com>
2013-10-15 16:19:11 -0400

    Cleaning up for the presentation

--- src/dwarffortress/presentation.clj
+++ src/dwarffortress/presentation.clj
@@ -48,8 +48,89 @@
 ;; Macros allow for the expansion of code -without- the
 ;; increase in stack depth and associated attributes
 
+;; ---- Game probability macros ----
 
-;; Anaphoric Macros
+;; revert to just chance after figuring out what is wrong with '50% and '50x
+(defn chance-fn [x]
+  (> x (rand-int 100)))
+
+(defmacro chance [x]
+  (let [y (Integer/parseInt (clojure.string/replace (str x) "%" ""))]
+    `(chance-fn ~y)))
+;; (chance %50) => (chance-fn 50)
+
+(defmacro make-percents []
+  "(ifN x y) returns x N% of the time, but ensures conditional evaluation, like 'if'"
+  `(list ~@(map (fn [num] 
+            (let [macro-name (symbol (str "if" num))]
+              `(defmacro ~macro-name [x# y#]
+                 `(if (> ~~num (rand-int 100)) ~x# ~y#))))
+          (range 100))))
+;; (make-percents) => (defmacro if0 ...) (defmacro if1 ...) ...
+;; (pprint (macroexpand '(if1 :x :y)))
+
+
+
+
+
+
+
+
+
+;; But this function is lame!
+
+
+
+;; Think about what you want it to look like _first_
+
+(if-chance 50% :a 45% :b 5% :c)
+
+(if-percent 50 :a, 45 :b, 5 :c)
+
+(if-percent [50 :a] [45 :b] [5 :c])
+(give-me [50 chance of :a]
+         [45 chance of :b]
+         [5 chance of :c])
+'(50 x)
+
+
+
+
+
+(defn if-percent-fn [& n]
+  "(if-percent-fn 50 (fn [] :a) 49 (fn [] :b) 1 (fn [] :c)) returns :a 50% of the time, :b 49%, and :c 1%"
+  (if (odd? (count n))
+    (throw (Exception. "Must pass even num of args"))
+    (let [pairs (partition 2 n)
+          sum (reduce + (filter pos? (map first pairs)))]
+      (if (= 100 sum)
+        (let [roll (inc (rand-int 100))]
+          (loop [current-sum 0
+                 items pairs]
+            (let [[next-percent next-val] (first items)
+                  current-sum (+ current-sum next-percent)]
+              (if (<= roll current-sum)
+                (next-val)
+                (recur current-sum (rest items))))))
+        (throw (Exception. (str "Nums: " sum " didn't equal 100" )))))))
+
+(defmacro if-percent [& n]
+  "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%"
+  (if (even? (count n))
+    (let [pairs (partition 2 n)
+          pairs (mapcat (fn [[pct act]] [pct `(fn [] ~act)]) pairs)]
+      `(if-percent-fn ~@pairs))))
+
+#_(float (average (map (fn [x] (if-percent 50 0 50 100))
+                 (range 1000))))
+
+(defn average [col]
+  (let [sum (reduce + col)
+        count (count col)]
+    (/ sum count)))
+
+
+;; ---- Anaphoric Macros ----
 (defmacro add-weird? [x]
   `(let [~'z 100]
      ~x))
@@ -72,54 +153,6 @@
                    body)]
      ~'_))
 
-
-(defn chance-fn [x]
-  (> x (rand-int 100)))
-(defmacro chance [x]
-  (let [y (Integer/parseInt (clojure.string/replace (str x) "%" ""))]
-    `(chance-fn ~y)))
-;; (chance %50) => 
-
-(defmacro make-percents []
-  "(ifN x y) returns x N% of the time, but ensures conditional evaluation, like 'if'"
-  `(list ~@(map (fn [num] 
-            (let [macro-name (symbol (str "if" num))]
-              `(defmacro ~macro-name [x# y#]
-                 `(if (> ~~num (rand-int 100)) ~x# ~y#))))
-          (range 100))))
-
-(defn if-percent-fn [& n]
-  "(if-percent-fn 50 (fn [] :a) 49 (fn [] :b) 1 (fn [] :c)) returns :a 50% of the time, :b 49%, and :c 1%"
-  (if (odd? (count n))
-    (throw (Exception. "Must pass even num of args"))
-    (let [pairs (partition 2 n)
-          sum (reduce + (filter pos? (map first pairs)))]
-      (if (= 100 sum)
-        (let [roll (inc (rand-int 100))]
-          (loop [current-sum 0
-                 items pairs]
-            (let [[next-percent next-val] (first items)
-                  current-sum (+ current-sum next-percent)]
-              (if (<= roll current-sum)
-                (next-val)
-                (recur current-sum (rest items))))))
-        (throw (Exception. (str "Nums: " sum " didn't equal 100" )))))))
-
-
-(defmacro if-percent [& n]
-  "(if-percent 50 :a 49 :b 1 :c) returns :a 50% of the time, :b 49%, and :c 1%"
-  (if (even? (count n))
-    (let [pairs (partition 2 n)
-          pairs (mapcat (fn [[pct act]] [pct `(fn [] ~act)]) pairs)]
-      `(if-percent-fn ~@pairs))))
-
-#_(float (average (map (fn [x] (if-percent 99 0 1 100))
-                 (range 1000))))
-
-(defn average [col]
-  (let [sum (reduce + col)
-        count (count col)]
-    (/ sum count)))
 
 ;; Transliteration of PG's aif
 ;; Anaphoric macro that binds to "it" the test result
