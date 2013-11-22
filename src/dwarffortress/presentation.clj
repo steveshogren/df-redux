898e5176ef90ed959bf2bc62e410d99ada401501
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-22 16:14:11 -0500

    Some scratchpad work

--- src/dwarffortress/presentation.clj
+++ src/dwarffortress/presentation.clj
@@ -22,6 +22,36 @@
 (defn fib2 [n]
   (map first (take n (iterate (fn [[x y]] [y (+ x y)]) [1 2]))))
 
-(fib2 10)
+(fib3 10)
 
+(defn fib3 [n]
+  (->> (iterate (fn [[x y]] [y (+ x y)]) [1 2])
+       (take n)
+       (map first)))
+
+
+;; C-h r - emacs manual --> best emacs book
+;; C-h i - top level help --> "elisp intro" best book on elisp
+;; C-h m - modes enabled
 ;; => [1 2 3 5 8 13 21 34 55 89]
+
+
+(defprotocol Player
+  (choose [p])
+  (update-strategy [p me you]))
+
+(defrecord Stubborn [choice]
+  Player
+  (choose [_] choice)
+  (update-strategy [this _ _] this))
+
+(defrecord Mean [last-winner]
+  Player
+  (choose [_]
+    (if last-winner
+      last-winner
+      (random-choice)))
+  (update-strategy [_ me you]
+    (Mean. (when (iwon? me you) me))))
+
+
