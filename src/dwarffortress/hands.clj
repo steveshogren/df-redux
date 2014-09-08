a52e4ddcd9f0fcf80a40205fb32c0f6094d4d906
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-08 07:40:09 -0400

    Adding a new hand guesser to keep sharp

---
+++ src/dwarffortress/hands.clj
@@ -0,0 +1,10 @@
+(ns dwarffortress.hands)
+
+(+ 1 1)
+
+(defn identify [h]
+  (let [suit (keyword (first (map str (take 1 h))))
+        num  (reduce str (drop 1 h))]
+    [suit num]))
+
+(identify "H12")
