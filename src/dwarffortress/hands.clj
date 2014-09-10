ad7fd9e25630b4d3c100953b400ce789aa895d56
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-10 07:12:32 -0400

    Adding the cases for the x-of-a-kind using copy paste

--- src/dwarffortress/hands.clj
+++ src/dwarffortress/hands.clj
@@ -8,16 +8,54 @@
     [suit num]))
 
 (defn is-two-of-a-kind [cs]
-  (let [nums (map second cs)]
-    (group-by [1 2 4 5])))
+  (let [nums (map second cs)
+        grouped (group-by (fn [x] x) nums)
+        grouped-counts (map (fn [[num list]] [(count list) num]) grouped)
+        twos (filter (fn [[count _]] (= 2 count)) grouped-counts)]
+    (if (empty? twos)
+      false
+      twos)))
+(defn is-three-of-a-kind [cs]
+  (let [nums (map second cs)
+        grouped (group-by (fn [x] x) nums)
+        grouped-counts (map (fn [[num list]] [(count list) num]) grouped)
+        twos (filter (fn [[count _]] (= 3 count)) grouped-counts)]
+    (if (empty? twos)
+      false
+      twos)))
 
-(is-two-of-a-kind h)
+(defn is-four-of-a-kind [cs]
+  (let [nums (map second cs)
+        grouped (group-by (fn [x] x) nums)
+        grouped-counts (map (fn [[num list]] [(count list) num]) grouped)
+        twos (filter (fn [[count _]] (= 4 count)) grouped-counts)]
+    (if (empty? twos)
+      false
+      twos)))
 
 (defn rank [cs]
   (let [cs (map identify cs)]
     (cond (is-two-of-a-kind cs) :two-kind
-          :else :nada)))
-(rank h)
-(def h  ["H2" "D2" "S4" "C5" "C8"])
-(identify "H12")
+          (is-three-of-a-kind cs) :three-kind
+          (is-four-of-a-kind cs) :four-kind
+          :else :highcard)))
 
+(= [:H "12"] (identify "H12"))
+(= :two-kind (rank ["H2" "D2" "S4" "C5" "C8"]))
+(= :highcard (rank ["H7" "D2" "S4" "C5" "C8"]))
+(= :three-kind (rank ["H7" "D7" "S7" "C5" "C8"]))
+(= :four-kind (rank ["H7" "D7" "S7" "C7" "C8"]))
+(= :two-pair (rank ["H7" "D7" "S9" "C9" "C8"]))
+
+(defmacro trace [n f]
+  `(do
+     (println ~n ":" ~f)
+     ~f))
+
+(defn wrap-args-with-trace [[symb val]]
+  [symb (list `trace (str "let-" symb) val)])
+
+(defmacro tracelet [args & body]
+  (let [arg-pairs (partition 2 args)
+        new-bindings (vec (mapcat wrap-args-with-trace arg-pairs))]
+    `(let ~new-bindings ~@body)))
