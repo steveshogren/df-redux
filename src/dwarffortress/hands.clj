4862abad72dec2b99e223792c8d36b9d27b60781
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-11 07:12:50 -0400

    Adding a straight

--- src/dwarffortress/hands.clj
+++ src/dwarffortress/hands.clj
@@ -4,11 +4,11 @@
 
 (defn identify [h]
   (let [suit (keyword (first (map str (take 1 h))))
-        num  (reduce str (drop 1 h))]
+        num (read-string (str (reduce str (drop 1 h))))]
     [suit num]))
 
 (defn is-x-of-a-kind [cs x]
-  (tracelet [nums (map second cs)
+  (let [nums (map second cs)
         grouped (group-by (fn [x] x) nums)
         grouped-counts (map (fn [[num list]] [(count list) num]) grouped)
         twos (filter (fn [[count _]] (= x count)) grouped-counts)]
@@ -30,11 +30,24 @@
 (defn is-flush [cs]
   (apply = (map first cs)))
 
+(defn is-straight [cs]
+  (tracelet [nums (map second cs)
+             sorted (sort nums)
+             start-num (first sorted)
+             expected-straight (range start-num (+ 5 start-num))
+             match? (reduce (fn [x y] (and (= true x)
+                                           (= x y)))
+                            (map = sorted expected-straight))]
+            (if match?
+              sorted
+              false)))
+
 (defn rank [cs]
   (let [cs (map identify cs)]
     (cond 
           (is-flush cs) :flush
           (is-two-pair cs) :two-pair
+          (is-straight cs) :straight
           (is-full-house cs) :full-house
           (is-two-of-a-kind cs) :two-kind
           (is-three-of-a-kind cs) :three-kind
@@ -42,13 +55,14 @@
           :else :highcard)))
 
 (= 
- (= [:H "12"] (identify "H12"))
+ (= [:H 12] (identify "H12"))
  (= :two-kind (rank ["H2" "D2" "S4" "C5" "C8"]))
  (= :highcard (rank ["H7" "D2" "S4" "C5" "C8"]))
  (= :three-kind (rank ["H7" "D7" "S7" "C5" "C8"]))
  (= :two-pair (rank ["H7" "D7" "S5" "C5" "C8"])) 
  (= :full-house (rank ["H7" "D7" "S7" "C5" "C5"])) 
  (= :flush (rank ["H7" "H3" "H9" "H5" "H2"])) 
+ (= :straight (rank ["H3" "D7" "S5" "C6" "C4"])) 
  (= :four-kind (rank ["H7" "D7" "S7" "C7" "C8"])))
 
 
