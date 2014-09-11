8a3f186a2973f29df16c3e7d902da0f5bb11b6a0
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-11 06:53:38 -0400

    Adding two pair

--- src/dwarffortress/hands.clj
+++ src/dwarffortress/hands.clj
@@ -7,24 +7,28 @@
         num  (reduce str (drop 1 h))]
     [suit num]))
 
-
 (defn is-x-of-a-kind [cs x]
-  (let [nums (map second cs)
+  (tracelet [nums (map second cs)
         grouped (group-by (fn [x] x) nums)
         grouped-counts (map (fn [[num list]] [(count list) num]) grouped)
         twos (filter (fn [[count _]] (= x count)) grouped-counts)]
     (if (empty? twos)
-      false
+      []
       twos)))
-(defn is-two-of-a-kind [cs] (is-x-of-a-kind cs 2))
-(defn is-three-of-a-kind [cs] (is-x-of-a-kind cs 3)) 
-(defn is-four-of-a-kind [cs] (is-x-of-a-kind cs 4))
+
+(defn is-two-of-a-kind [cs] (= 1 (count (is-x-of-a-kind cs 2))))
+(defn is-three-of-a-kind [cs] (= 1 (count (is-x-of-a-kind cs 3)))) 
+(defn is-four-of-a-kind [cs] (= 1 (count (is-x-of-a-kind cs 4))))
+
+(defn is-two-pair [cs]
+  (= 2 (count (is-x-of-a-kind cs 2))))
 
 (defn rank [cs]
   (let [cs (map identify cs)]
     (cond (is-two-of-a-kind cs) :two-kind
           (is-three-of-a-kind cs) :three-kind
           (is-four-of-a-kind cs) :four-kind
+          (is-two-pair cs) :two-pair
           :else :highcard)))
 
 (= 
@@ -32,6 +36,7 @@
  (= :two-kind (rank ["H2" "D2" "S4" "C5" "C8"]))
  (= :highcard (rank ["H7" "D2" "S4" "C5" "C8"]))
  (= :three-kind (rank ["H7" "D7" "S7" "C5" "C8"]))
+ (= :two-pair (rank ["H7" "D7" "S5" "C5" "C8"])) 
  (= :four-kind (rank ["H7" "D7" "S7" "C7" "C8"])))
 
 
