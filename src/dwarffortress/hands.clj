de886bfded79f34a59c250c122a4916346d5d1ae
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-12 08:43:31 -0400

    Adding the ability for an ace to be a high or low card

--- src/dwarffortress/hands.clj
+++ src/dwarffortress/hands.clj
@@ -1,6 +1,19 @@
 (ns dwarffortress.hands)
 
-(def suit-ranks {:H 1 :S 2 :C 3 :D 4})
+(defmacro trace [n f]
+  `(do
+     (println ~n ":" ~f)
+     ~f))
+
+(defn wrap-args-with-trace [[symb val]]
+  [symb (list `trace (str "let-" symb) val)])
+
+(defmacro tracelet [args & body]
+  (let [arg-pairs (partition 2 args)
+        new-bindings (vec (mapcat wrap-args-with-trace arg-pairs))]
+    `(let ~new-bindings ~@body)))
+
+(def suit-ranks {:H 1 :S 2 :C 3 "D" 4})
 
 (defn identify [h]
   (let [suit (keyword (first (map str (take 1 h))))
@@ -30,9 +43,8 @@
 (defn is-flush [cs]
   (apply = (map first cs)))
 
-(defn is-straight [cs]
-  (tracelet [nums (map second cs)
-             sorted (sort nums)
+(defn i-is-straight [nums]
+  (tracelet [sorted (sort nums)
              start-num (first sorted)
              expected-straight (range start-num (+ 5 start-num))
              match? (reduce (fn [x y] (and (= true x)
@@ -41,6 +53,17 @@
             (if match?
               sorted
               false)))
+
+(defn is-straight [cs]
+  (let [nums (map second cs)]
+    (if (= 1 (count (filter #(= 1 %) nums)))
+      (tracelet [low-ace (i-is-straight nums)
+                 high-ace (i-is-straight (conj (filter #(not= 1 %) nums) 14))]
+                (cond low-ace low-ace
+                      high-ace high-ace
+                      :else false))
+      (i-is-straight nums))))
+
 
 (defn is-straight-flush [cs]
   (and (is-straight cs)
@@ -59,6 +82,7 @@
           (is-four-of-a-kind cs) :four-kind
           :else :highcard)))
 
+;; group-by, map, reduce, cond, if, range, sort, first, second, =, keyword, read-string, and
 (= 
  (= [:H 12] (identify "H12"))
  (= :two-kind (rank ["H2" "D2" "S4" "C5" "C8"]))
@@ -68,18 +92,7 @@
  (= :full-house (rank ["H7" "D7" "S7" "C5" "C5"])) 
  (= :flush (rank ["H7" "H3" "H9" "H5" "H2"])) 
  (= :straight (rank ["H3" "D7" "S5" "C6" "C4"])) 
+ (= :straight (rank ["H1" "D10" "S11" "C12" "C13"])) 
  (= :straight-flush (rank ["H3" "H7" "H5" "H6" "H4"])) 
  (= :four-kind (rank ["H7" "D7" "S7" "C7" "C8"])))
 
-(defmacro trace [n f]
-  `(do
-     (println ~n ":" ~f)
-     ~f))
-
-(defn wrap-args-with-trace [[symb val]]
-  [symb (list `trace (str "let-" symb) val)])
-
-(defmacro tracelet [args & body]
-  (let [arg-pairs (partition 2 args)
-        new-bindings (vec (mapcat wrap-args-with-trace arg-pairs))]
-    `(let ~new-bindings ~@body)))
