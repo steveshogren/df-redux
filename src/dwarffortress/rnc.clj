afaca53eac99c101c3d05938d4669a8cc6aa6abe
Steve Shogren <steve.a.shogren@gmail.com>
2014-10-02 07:16:05 -0400

    Partially fixing the lookahead

--- src/dwarffortress/rnc.clj
+++ src/dwarffortress/rnc.clj
@@ -15,14 +15,21 @@
 
 (def nums {\I 1 \V 5 \X 10})
 
-(defn parse-doubles [s]
-  (let [pairs (partition 2 (map parse-single s))]
-    (map (fn [[f s]] (if (> f s) (- s f) s)) pairs)
-    ))
-
 (defn parse-single [s]
   (get nums s))
 
+(defn parse-doubles [s]
+  (tracelet [pairs (partition 2 1 (map parse-single (seq s)))
+        [f s] (first pairs)
+        first-val (if (> f s) f 0)
+        rest-vals (map (fn [[f s]] (if (< f s) (- s f) s)) pairs)]
+    (reduce + first-val rest-vals)))
+
 (defn parse [s]
-  (reduce + (map parse-single (seq s))))
+  (if (= 1 (count s))
+    (reduce + (map parse-single (seq s)))
+    (parse-doubles s)))
+
+;; (parse "")
+
 
