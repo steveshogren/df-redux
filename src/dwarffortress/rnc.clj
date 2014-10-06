36d959094a92313790ca9e7ca7f81cabd307a65d
Steve Shogren <steve.a.shogren@gmail.com>
2014-10-06 07:33:19 -0400

    Adding additional numbers

--- src/dwarffortress/rnc.clj
+++ src/dwarffortress/rnc.clj
@@ -1,8 +1,8 @@
 (ns dwarffortress.rnc
   (:require [dwarffortress.trace :refer [trace tracelet]]))
 
-(def nums {\I 1 \V 5 \X 10})
-(def num-s {1 "I" 5 "V" 10 "X"})
+(def nums {\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000})
+(def num-s {1 "I" 5 "V" 10 "X" 50 "L" 100 "C" 500 "D" 1000 "M"})
 
 (defn parse-single [c] (get nums c))
 
