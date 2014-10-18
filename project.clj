06309efbc0ab118a9078d719b7646fef30fff175
Steve Shogren <steve.a.shogren@gmail.com>
2014-10-18 15:35:41 -0400

    Finishing the part that detects missing days

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [deft "0.1.1"]
                  [seesaw "1.4.3"]
+                 [clj-time "0.8.0"]
+                 [org.clojure/tools.trace "0.7.8"]
                  [midje "1.6.3"]]
   :plugins [[lein-midje "3.1.1"]
             [cider/cider-nrepl "0.7.0"]]
--- src/dwarffortress/git_data.clj
+++ src/dwarffortress/git_data.clj
@@ -4,7 +4,15 @@
   (:require [clojure.string :as s]
             [clojure.java.io :as io]
             [clojure.set :as set]
+            [clojure.tools.trace :refer [trace]]
+            [dwarffortress.trace :refer [tracelet]]
+            [clj-time.core :as t]
+            [clj-time.local :as l]
+            [clj-time.format :as f]
+            [clj-time.coerce :as c]
             [clojure.java.shell :refer [sh]]))
+
+(l/local-now)
 
 (defn from-unix-time [t]
   (java.util.Date. t))
@@ -33,6 +41,18 @@
 
 (def current-year (get-year (Date.)))
 
+(defn add-days-to-date [days date]
+  (let [cal (Calendar/getInstance)]
+    (.setTime cal date)
+    (.add cal Calendar/DATE days)
+    (.setTimeZone cal (java.util.TimeZone/getDefault))
+    (let [expected-day (.get cal Calendar/DATE)
+          inc-time-by (rand-int 60)]
+      ;; (.add cal java.util.Calendar/MINUTE inc-time-by)
+      (.getTime cal))))
+
+(add-days-to-date 1 (Date.))
+
 (defn get-day-map-from-date 
   ([d] (get-day-map-from-date d 0))
   ([d days-back]
@@ -45,13 +65,16 @@
 (defn get-date-add-days [days]
   (get-day-map-from-date (Date.) days))
 
-(defn dates-by-day [date-lists]
+(defn group-dates-by-day [date-lists]
   (reduce (fn [ac ne] (assoc ac (:day ne) (:date ne)))
           {}
           date-lists))
 
 (defn get-expected-date-map [in-past]
   (map #(get-date-add-days (- %)) (range in-past)))
+
+(map #(c/from-long (* 1000 (parse-long %)))
+     (get-git-logs "/home/jack/programming/clojadelphia-macros/.git/"))
 
 (defn get-dates-from-dir [dir]
   (map get-day-map-from-date
@@ -65,18 +88,17 @@
 (defn get-last-x-days [day-map num]
   (reverse (take num ((comp reverse sort) (keys day-map)))))
 
-
-
 (defn find-missing-days [days-back]
-  (let [expected-days-map (dates-by-day (get-expected-date-map days-back))
-        git-dates (dates-by-day (mapcat #(get-dates-from-dir %)
-                                    (conj (get-git-paths)
-                                          "/home/jack/.emacs.d/.git")))
+  (tracelet [expected-days-map (group-dates-by-day (get-expected-date-map days-back))
+        git-dates (group-dates-by-day (mapcat #(get-dates-from-dir %)
+                                              (conj (get-git-paths)
+                                                    "/home/jack/.emacs.d/.git")))
         expected-days (get-last-x-days (only-this-years expected-days-map) days-back) 
-        act-days (get-last-x-days git-dates days-back)
-        missing-days (set/difference (set expected-days) (set act-days))]
-    (if (seq? missing-days)
+        actual-days (get-last-x-days git-dates days-back)
+        missing-days (set/difference (set expected-days) (set actual-days))]
+    (when (not-empty missing-days)
       (let [earliest-missing (apply min missing-days)]
         (get expected-days-map earliest-missing)))))
 
 (find-missing-days 20)
+
