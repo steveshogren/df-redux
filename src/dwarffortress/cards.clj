817368ac85a495d3dbf1c627b8b1ab4a792570bc
Steve Shogren <steve.a.shogren@gmail.com>
2014-11-15 18:02:46 -0500

    Creating the deck for the game

--- src/dwarffortress/cards.clj
+++ src/dwarffortress/cards.clj
@@ -18,3 +18,43 @@
      (pct-of (:pawn-shop location-cards) count) :pawn-shop 
      (pct-of (:gas-station location-cards) count) :gas-station)))
 
+(p/if-percent [50 :a 50 :b])
+
+(def item-cards {
+                 :baricade-hearts-8 2
+                 :baricade-hearts-4 2
+                 :baricade-hearts-6 2
+                 :baricade-hearts-10 2
+                 :baricade-diamonds-8 2
+                 :baricade-diamonds-4 2
+                 :baricade-diamonds-6 2
+                 :baricade-diamonds-10 2
+                 :baricade-spades-8 2
+                 :baricade-spades-4 2
+                 :baricade-spades-6 2
+                 :baricade-spades-10 2
+                 :baricade-clubs-8 2
+                 :baricade-clubs-4 2
+                 :baricade-clubs-6 2
+                 :baricade-clubs-10 2
+
+                 :weapon-hearts-5 2
+                 :weapon-hearts-7 2
+                 :weapon-diamonds-5 2
+                 :weapon-diamonds-7 2
+
+                 :weapon-mod-clubs-5 2
+                 :weapon-mod-clubs-7 2
+                 :weapon-mod-spades-5 2
+                 :weapon-mod-spades-7 2
+                 :weapon-mod-diamonds-5 2
+                 :weapon-mod-diamonds-7 2
+                 :weapon-mod-hearts-5 2
+                 :weapon-mod-hearts-7 2
+
+                 :food 16
+                 :ammo 16
+                 :gas 8 
+                 :car-part 8
+
+                 })
